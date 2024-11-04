8a70340e838ee8a29492a1cd05bf0a9bfeb161e8
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-11-04 21:43:56 +0500

    кнопочки

--- real.py
+++ real.py
@@ -1,17 +1,59 @@
 import asyncio
-from time import sleep
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-async def start_strongman(name, power):
-    print(f'Силач {name} начал соревнования.')
-    for i in range(5):
-        await asyncio.sleep(1/power)
-        print(f'Силач {name} поднял {i+1} шар' )
-    print(f'Силач {name} закончил соревнования.')
-async def sorevnovanie():
-    first=asyncio.create_task(start_strongman('Pasha', 3))
-    second=asyncio.create_task(start_strongman('Denis', 4))
-    third=asyncio.create_task(start_strongman('Apollon', 5))
-    await first
-    await second
-    await third
-asyncio.run(sorevnovanie())
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb=ReplyKeyboardMarkup(resize_keyboard=True)
+button=KeyboardButton(text='Рассчитать')
+button1=KeyboardButton(text='информация')
+kb.row(button, button1)
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
+
+
+
+class UserState(StatesGroup):
+    age=State()
+    growth=State()
+    weight=State()
+
+@dp.message_handler(text=['Рассчитать'])
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await UserState.weight.set()
+    await state.update_data(weight=message.text)
+    data=await state.get_data()
+    res=10*int(data['weight'])+6.25*int(data['growth'])-5*int(data['age'])+5
+    await message.answer(str(res))
+    await state.finish()
+
+
+@dp.message_handler()
+async def unstart_messge(message):
+    await message.answer('введите команду /start для начала работы бота')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
